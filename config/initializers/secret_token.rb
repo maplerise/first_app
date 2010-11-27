449d5ee7bf6589b9a808e9d2f6379dc0142a220d
Skip Hovsmith <skip@maplerise.com>
2010-11-26 22:12:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a7a51492cd2ea20bf03adfcb2a69d7b84770064f0f602518e0ac6a7a3e0101bc0fefb71df077bc3fc92e9db7bd811bcc0eca708a755b96c48f05d04a7d3b32b5'
